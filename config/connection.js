13722054089f4be346ca524a073e82924427f16f
Vanessa Lane <vlane@23andme.com>
2020-08-17 23:05:34 -1000

    add missing bracket to connection.js

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,10 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     sequelize = new Sequelize('just_tech_news_db', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 
 module.exports = sequelize;
