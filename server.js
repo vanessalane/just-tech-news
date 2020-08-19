582564c7797eb2dfa092fd3a84b12286329eee22
Vanessa Lane <vlane@23andme.com>
2020-08-18 15:58:07 -1000

    add homepage view and restructure app

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./routes');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.static((path.join(__dirname, 'public'))));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// turn on routes
 app.use(routes);
 
 // turn on connection to db and server
