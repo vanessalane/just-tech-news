c00cad73b089c1b867b645e188fffbed1f7150d3
Vanessa Lane <vlane@23andme.com>
2020-08-19 22:07:03 -1000

    add conditional statements

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -48,7 +48,10 @@ router.get('/post/:id', (req, res) => {
       const post = dbPostData.get({ plain: true });
 
       // pass data to template
-      res.render('single-post', { post });
+      res.render('single-post', {
+        post,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch(err => {
       console.log(err);
@@ -84,7 +87,10 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
       console.log(dbPostData[0])
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('homepage', { posts });
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch(err => {
       console.log(err);
