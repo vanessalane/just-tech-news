8e191834d5c3aa86eac766e7e1f2a0a33df1aa82
Vanessa Lane <vlane@23andme.com>
2020-08-19 21:17:36 -1000

    add single post and upvote functionality

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -48,16 +48,5 @@ async function signupFormHandler(event) {
     }
 }
 
-router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
-    }
-    else {
-        res.status(404).end();
-    }
-});
-
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
